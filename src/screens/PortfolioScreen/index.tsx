a4a43e145be63ef2aa24627d6af1c4cb4606434d
Harsh Dave <harsh.dave@digiplusit.com>
2021-04-25 22:20:08 +0530

    Completed Portfolio Screen UI

--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -1,10 +1,100 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, FlatList } from 'react-native';
+import PortfolioCoin from '../../components/PortfolioCoin/Index';
+import styles from './styles';
+const image = require('../../../assets/images/Saly-10.png');
+const PortfolioCoins = [
+    {
+        id: '1',
+        name: 'virtual Dollars',
+        symbol: 'USD',
+        amount: 69.420,
+        valueUSD: 69.420,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    },
+    {
+        id: '2',
+        name: 'BitCoin',
+        symbol: 'USD',
+        amount: 1.12,
+        valueUSD: 59.420,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    },
+    {
+        id: '3',
+        name: 'Etherium',
+        symbol: 'ETH',
+        amount: 30,
+        valueUSD: 30.150,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    },
+    {
+        id: '4',
+        name: 'Etherium',
+        symbol: 'ETH',
+        amount: 30,
+        valueUSD: 30.150,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    },
+    {
+        id: '5',
+        name: 'Etherium',
+        symbol: 'ETH',
+        amount: 30,
+        valueUSD: 30.150,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    },
+    {
+        id: '6',
+        name: 'Etherium',
+        symbol: 'ETH',
+        amount: 30,
+        valueUSD: 30.150,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    },
+    {
+        id: '7',
+        name: 'Etherium',
+        symbol: 'ETH',
+        amount: 30,
+        valueUSD: 30.150,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    },
+    {
+        id: '8',
+        name: 'Etherium',
+        symbol: 'ETH',
+        amount: 30,
+        valueUSD: 30.150,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    },
+    {
+        id: '9',
+        name: 'Etherium',
+        symbol: 'ETH',
+        amount: 30,
+        valueUSD: 30.150,
+        image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'
+    }
+
+
+]
 
 const PortfolioScreen = () => {
      return (
-         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-             <Text>PortfolioScreen Component</Text>
+         <View style={styles.root}>
+         <Image style={styles.image} source={image} />
+            <View style={styles.balanceContainer}>
+                <Text style={styles.label}>Portfolio Balance</Text>
+                <Text style={styles.balance}>$69.420</Text>
+            </View>
+
+            <FlatList 
+            style={{width: '100%'}}
+                data={PortfolioCoins} 
+                renderItem={({item}) => <PortfolioCoin portfolioCoin= {item}/>}
+            />
+
          </View>
      )
 }
