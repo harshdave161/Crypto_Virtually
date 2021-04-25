21ff3d65b6aad10aa333c35d50205df81c38fd53
Harsh Dave <harsh.dave@digiplusit.com>
2021-04-25 20:14:52 +0530

    intiall Commit with setup project task && home Screen Ui && Bottom tab Navigation Have been completed According to notion Board !! Happy Coding

---
+++ types.tsx
@@ -0,0 +1,17 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Profile: undefined;
+  Market: undefined;
+  Portfolio: undefined;
+  Ranking: undefined;
+};
